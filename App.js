f2baaedb9dce5bf4fa6cfec3a55852bc663cb9ea
unknown <brenomax02@gmail.com>
2020-11-13 18:09:49 -0300

    aula 2 finalizada

--- App.js
+++ App.js
@@ -2,12 +2,46 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Navigations
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+//Pages
+import Login from './pages/Login'
+import Home from './pages/Home'
+import AsyncStorage from '@react-native-community/async-storage';
+
+const Autenticado = () => {
+  return(
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+      </Drawer.Navigator>
+  )
+}
+
+const Logout = ( {navigation} ) => {
+  return(
+    <View>
+      <Text>Deseja sair?</Text>
+      <button onPress={() => {
+        AsyncStorage.push('Login');
+      }} title="Sair"></button>
+    </View>
+  )
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown : false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Autenticado" component={Autenticado} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
